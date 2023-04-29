3a230684f22e1f3f941f216890fe195a65d14e77
HanterGan <gonidzze@gmail.com>
2023-04-29 22:12:46 +0300

    Add formik and tailwind-forms

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import {
   BrowserRouter as Router, Routes, Route, useLocation, Navigate,
 } from 'react-router-dom';
+import Navbar from '../components/Navbar.jsx';
+import Login from './Login.jsx';
+
 
 function App(props) {
+
   return (
-    <div>
+    <div className="md:container md:mx-auto bg-slate-300 h-screen">
+      <Navbar />
       <h1 className="text-3xl font-bold underline">
         Application for fetching data.
       </h1>
       <Router>
         <Routes>
           <Route path={'/'} element={<p>Home</p>} />
-          <Route path={'/login'} element={<p>Login</p>} />
+          <Route path={'/login'} element={<Login />} />
           <Route path={'/send'} element={<p>Send data</p>} />
           <Route path={'/manageusers'} element={<p>Manage users</p>} />
           <Route path={'/statistics'} element={<p>Statistics</p>} />
