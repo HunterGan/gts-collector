67511325b1f25c1fae7e11753a2723974733de1b
HanterGan <gonidzze@gmail.com>
2023-04-30 16:04:52 +0300

    Add validation yup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { useFormik } from 'formik';
+import * as yup from 'yup';
 
-function Login(props) {
+function Login() {
   const loginForm = useFormik({
     initialValues: {
-      userName: '',
+      username: '',
       password: '',
     },
-    onSubmit: (values) => {
-      console.log(values);
+    validationSchema: yup.object().shape({
+      username: yup.string().required('usernameRequired'),
+      password: yup.string().required('passwordRequired'),
+    }),
+    onSubmit: () => {
+      console.log('working');
     }
   })
   return (
@@ -16,7 +21,7 @@ function Login(props) {
       <div className='grid grid-row-3 gap-5 max-w-3xl w-full border text-xl'>
         <div >LOGO</div>
         <h1>Sign in to your account</h1>
-        <form className=''>
+        <form className='' onSubmit={loginForm.handleSubmit}>
           <input type="text" className="form-input" />
           <input type="password" className="form-input" />
           <button type='submit'>apply</button>
